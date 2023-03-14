48235234301a73a2be93979e87115ab48a597d34
Adegboyega <gboyega.banjo@gmail.com>
2023-03-14 19:10:08 +0100

    image unoptimized

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -34,6 +34,7 @@ export default function Home() {
                 width={100}
                 height={24}
                 priority
+                unoptimized
               />
             </a>
           </div>
