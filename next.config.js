1e61498df95455edee2c8fd72a6e63e5c07145dd
Adegboyega <gboyega.banjo@gmail.com>
2023-06-12 22:25:22 +0100

    next config

--- next.config.js
+++ next.config.js
@@ -11,6 +11,20 @@ const nextConfig = {
     ],
   },
   trailingSlash: true,
+  exportPathMap: async function (
+    defaultPathMap,
+    { dev, dir, outDir, distDir, buildId }
+  ) {
+    return {
+      '/': { page: '/' },
+      '/blog': { page: '/blog' },
+      '/courses': { page: '/courses' },
+      '/about_us': { page: '/about_us' },
+      '/services': { page: '/services' },
+      '/interviews': { page: '/interviews' },
+      '/contact_us': { page: '/contact_us' },
+    }
+  },
 }
 
 module.exports = nextConfig
